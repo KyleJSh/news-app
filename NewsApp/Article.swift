30593a6811109b61a46ada80dc4450e6cc5daa3c
Kyle Sherrington <kylejsherrington@gmail.com>
2021-03-01 15:05:24 -0800

    Initial Commit

---
+++ NewsApp/Article.swift
@@ -0,0 +1,19 @@
+//
+//  Article.swift
+//  NewsApp
+//
+//  Created by Kyle Sherrington on 2021-03-01.
+//
+
+import Foundation
+
+struct article: Codable {
+    
+    var author:String?
+    var title:String?
+    var description:String?
+    var url:String?
+    var urlToImage:String?
+    var PublishedAt:String?
+    
+}
